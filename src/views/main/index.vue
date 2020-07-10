a3b5b5ed660fdd3d3fa2c991625bd123d937e049
yzcheng90 <yzcheng90@qq.com>
2020-07-10 17:01:00 +0800

    Initial commit

---
+++ src/views/main/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>这是主页面</div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {}
+  },
+  computed: {
+
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
